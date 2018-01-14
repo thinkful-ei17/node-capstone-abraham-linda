49d124edd322f2fc2806eefd61922c0889573e9f
Linda Foo <linda.foo.dev@gmail.com>
2018-01-14 12:46:41 -0800

    testing in progress - seed data works

--- items/v1/test.js
+++ items/v1/test.js
@@ -5,15 +5,15 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const faker = require('faker'); //library that helps with generating random seedData
-const mongoose = require('mongoose'); //library to help with db interaction (knex)
+const mongoose = require('mongoose'); //library to help with db interaction
 
 // this makes the expect syntax available throughout
 // this module
 const expect = chai.expect;
 
-const { Items } = require('./model');
+const { Item } = require('./model');
 const {app, runServer, closeServer} = require('../../server');
-const {TEST_DATABASE_URL} = require('../../config');
+const {DATABASE_URL_TEST} = require('../../config');
 
 chai.use(chaiHttp);
 
@@ -30,12 +30,12 @@ function seedItemData() {
     seedData.push(generateItemData());
   }
   // this will return a promise
-  return Items.insertMany(seedData);
+  return Item.insertMany(seedData);
 }
 
 
 function randomize(array) {
-  return array[Math.floor(Math.random()*i.length)];
+  return array[Math.floor(Math.random()*array.length)];
 }
 
 function generateRandomTypeAndStatus(){
@@ -75,27 +75,22 @@ function generateItemData() {
     description: faker.lorem.sentence(),
     postedBy: pName,
     acceptedBy: aName,
-    status:randomized.status,
-
-    author: {
-      firstName: faker.name.firstName(),
-      lastName: faker.name.lastName()
-    },
-    title: faker.random.words(),
-    content: faker.lorem.sentence()
+    status: randomized.status,
   };
 }
+
+
 
 // this function deletes the entire database.
 // we'll call it in an `afterEach` block below
 // to ensure data from one test does not stick
 // around for next one
-function tearDownDb() {
-  console.warn('Deleting database');
-  return mongoose.connection.dropDatabase();
-}
+// function tearDownDb() {
+//   console.warn('Deleting database');
+//   return mongoose.connection.dropDatabase();
+// }
 
-describe('BlogPosts API resource', function() {
+describe('Item API resource', function() {
 
   // we need each of these hook functions to return a promise
   // otherwise we'd need to call a `done` callback. `runServer`,
@@ -103,18 +98,18 @@ describe('BlogPosts API resource', function() {
   // so we return the value returned by these function calls.
   before(function() {
     console.log('API resource before ran');
-    return runServer(TEST_DATABASE_URL);
+    return runServer(DATABASE_URL_TEST);
   });
 
   beforeEach(function() {
     console.log('API resource beforeEach ran');
-    return seedBlogData();
+    return seedItemData();
   });
 
-  afterEach(function() {
-    console.log('API resource afterEach ran');
-    return tearDownDb();
-  });
+  // afterEach(function() {
+  //   console.log('API resource afterEach ran');
+  //   return tearDownDb();
+  // });
 
   after(function() {
     console.log('API resource after ran');
@@ -123,7 +118,7 @@ describe('BlogPosts API resource', function() {
 
   describe('GET endpoint', function() {
 
-    it('should return all existing blog posts', function() {
+    it('should return all existing items posted', function() {
     //  console.log('test false ran');
     // expect(false).be.false;
     // strategy:
@@ -136,7 +131,7 @@ describe('BlogPosts API resource', function() {
     // `.then()` calls below, so declare it here so can modify in place
       let res;
       return chai.request(app)
-        .get('/posts')
+        .get('/api/v1/items')
         .then(function(_res) {
         // so subsequent .then blocks can access response object
           res = _res;
@@ -144,45 +139,49 @@ describe('BlogPosts API resource', function() {
           // otherwise our db seeding didn't work
           console.log('what is body', res.body.length);
           expect(res.body).to.have.length.of.at.least(1); 
-          return BlogPost.count();
+          return Item.count();
         })
         .then(function(count) {
           console.log(count);
           expect(res.body).to.have.lengthOf(count);
         });
     });
+  }); 
+
+}); 
 
 
-    it('should return blogposts with right fields', function() {
-      // Strategy: Get back all blog posts, and ensure they have expected keys
-      //console.log('test true ran');
-      //expect(true).be.true;
-      let resBlog;
-      return chai.request(app)
-        .get('/posts')
-        .then(function(res) {
-          expect(res).to.have.status(200);
-          expect(res).to.be.json;
-          expect(res.body).to.be.a('array');
-          expect(res.body).to.have.length.of.at.least(1);
+    // it('should return blogposts with right fields', function() {
+    //   // Strategy: Get back all blog posts, and ensure they have expected keys
+    //   //console.log('test true ran');
+    //   //expect(true).be.true;
+    //   let resBlog;
+    //   return chai.request(app)
+    //     .get('/posts')
+    //     .then(function(res) {
+    //       expect(res).to.have.status(200);
+    //       expect(res).to.be.json;
+    //       expect(res.body).to.be.a('array');
+    //       expect(res.body).to.have.length.of.at.least(1);
 
-          res.body.forEach(function(blogpost) {
-            expect(blogpost).to.be.a('object');
-            expect(blogpost).to.include.keys(
-              'id', 'author', 'title', 'content','created');
-          });
-          resBlog = res.body[0];
-          return BlogPost.findById(resBlog.id);
-        })
-        .then(function(blogpost) {
-          expect(resBlog.id).to.equal(blogpost.id);
-          expect(resBlog.author).to.contain(blogpost.author.firstName);
-          expect(resBlog.title).to.equal(blogpost.title);
-          expect(resBlog.content).to.equal(blogpost.content); 
-        });
-    });
-  });
+    //       res.body.forEach(function(blogpost) {
+    //         expect(blogpost).to.be.a('object');
+    //         expect(blogpost).to.include.keys(
+    //           'id', 'author', 'title', 'content','created');
+    //       });
+    //       resBlog = res.body[0];
+    //       return BlogPost.findById(resBlog.id);
+    //     })
+    //     .then(function(blogpost) {
+    //       expect(resBlog.id).to.equal(blogpost.id);
+    //       expect(resBlog.author).to.contain(blogpost.author.firstName);
+    //       expect(resBlog.title).to.equal(blogpost.title);
+    //       expect(resBlog.content).to.equal(blogpost.content); 
+    //     });
+    // });
+  // });
 
+  /*
   describe('POST endpoint', function() {
     // strategy: make a POST request with data,
     // then prove that the blogpost we get back has
@@ -280,3 +279,4 @@ describe('BlogPosts API resource', function() {
     });
   });
 });
+*/
